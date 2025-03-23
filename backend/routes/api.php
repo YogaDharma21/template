4f1740ccbf257c698bd192d33d2dd9076cf1d551
YogaDharma21 <yogacode2006@gmail.com>
2025-03-23 16:35:34 +0800

    feat: add user registration endpoint to API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::post('/register', function (Request $request) {
     return response()->json([
         'halo' => 'halo',
+        'data' => $request->all(),
+        'status' => 200,
+        'time' => time(),
     ]);
 });
