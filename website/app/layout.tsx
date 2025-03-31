22e3c2fb2ab6a3471ee209a82c453d50e5753a30
YogaDharma21 <yogacode2006@gmail.com>
2025-03-31 20:35:30 +0800

    feat: add shadcn components

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <body
                 className={`${kanit.variable} ${geist.variable} antialiased`}
                 suppressHydrationWarning
