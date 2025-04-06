8f1670b3c965886fd3182b9bc968001db9a0e9a9
YogaDharma21 <yogacode2006@gmail.com>
2025-04-06 20:46:27 +0800

    feat: add sanctum on backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,11 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', function (Request $request) {
-    return response()->json([
-        'halo' => 'halo',
-        'data' => $request->all(),
-        'status' => 200,
-        'time' => time(),
-    ]);
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
